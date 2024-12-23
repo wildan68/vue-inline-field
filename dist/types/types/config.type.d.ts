66df8e3e9f78bef7e4864592ba3725971977edb9
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2024-12-23 21:12:30 +0700

    sadas

---
+++ dist/types/types/config.type.d.ts
@@ -0,0 +1,15 @@
+import type { Component } from 'vue';
+export type IComponent = {
+    id: string;
+    type: string;
+    component: string;
+    props?: Record<string, any>;
+    slots?: any;
+    config?: {
+        key: string;
+    };
+};
+export type IConfig = {
+    components: Record<IComponent["component"], Component | null>;
+    locale?: 'en' | 'id';
+};
